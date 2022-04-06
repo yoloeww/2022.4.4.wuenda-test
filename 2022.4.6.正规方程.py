44eada013e346fbddfca465f87eedb5f10a8825d
yolo <77111763+yoloeww@users.noreply.github.com>
2022-04-06 23:40:19 +0800

    Add files via upload

---
+++ 2022.4.6.正规方程.py
@@ -0,0 +1,9 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2022/4/6 12:59
+# @Author : yolo
+# @File : 2022.4.6.正规方程.py
+# @Software: PyCharm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
