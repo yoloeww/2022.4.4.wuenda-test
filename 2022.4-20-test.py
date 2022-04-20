9869e33cc6af049d1e0307822f130801fa5db765
yolo <77111763+yoloeww@users.noreply.github.com>
2022-04-20 21:18:24 +0800

    Add files via upload

---
+++ 2022.4-20-test.py
@@ -0,0 +1,8 @@
+# -*- codeing = utf-8 #-*-
+# @Time : 2022/4/2010:49 AM
+# @Author :yolo
+# @File :2022.4-20-test.py
+# @Software ：PyCharm
+
+from 2022-4-20-q import maze
+from 2022-4-20-brain import q
